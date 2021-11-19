5bb866d2bd3390ad9921783d371e7bc5653a3922
Александр Дегтярёв <91310650+Xu1iGaN4ik993@users.noreply.github.com>
2021-11-19 10:16:57 +0300

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,93 @@
+dictionaries = {
+    0 : "abcdefghijklmnopqrstuvwxyz0123456789",
+    1 : "abc0123456789ldefghijkmnopqrstuvwxyz"
+}
+
+def check_values(check_str):
+        
+
+    bool_flag = True
+    for char in check_str:
+        for d in dictionaries:
+            if char not in dictionaries[d]:
+                bool_flag = False
+    
+    return bool_flag
+
+class PolyWordClass(object):
+    
+    def __init__(self, s, key):
+
+        self.s = s
+        self.key = key
+
+        #Шифруем слово
+        self.main_encoded()
+
+        #Расшифровка слова
+        self.main_decoded()
+        
+
+    def char_encoded(self, d, char):
+        k = self.key
+        index = d.find(char)
+
+        #Если длина ключа больше самой строки
+        if k > len(d):
+            k = k-len(d)
+
+        if index+k >= len(d):
+            return d[index+k-len(d)]
+        else:
+            return d[index+k]
+
+    def char_decoded(self, d, char):
+        k = self.key
+        index = d.find(char)
+
+        #Если длина ключа больше самой строки
+        if k > len(d):
+            k = k-len(d)
+
+        if index-k >= len(d):
+            return d[index-k+len(d)]
+        else:
+            return d[index-k]
+
+    def main_encoded(self):
+
+        input_str = self.s
+        out = ""
+        for i in range(len(input_str)):
+            out += self.char_encoded(dictionaries[i % 2], input_str[i])
+        self.encoded = out
+
+    def main_decoded(self):
+
+        input_str = self.encoded
+        out = ""
+        for i in range(len(input_str)):
+            out += self.char_decoded(dictionaries[i % 2], input_str[i])
+        self.decoded = out
+
+def main():
+    try:
+
+        k = int(input("Введите сдвиг -> "))
+        s = str(input("Введите строку -> "))
+        
+    except:
+        print("Что-то пошло не так при вводе данных")
+        return
+    
+    if check_values(s) == False:
+        print("Нет символов введенной строки в исходном алфавите!")
+        return
+    
+    obj = PolyWordClass(s, k)
+    
+    print("\n**Шифрование**\nРезультат: " + obj.encoded)
+    print("\n**Расшифровка**\nРезультат: " + obj.decoded)
+
+if __name__ == "__main__":
+    main()
